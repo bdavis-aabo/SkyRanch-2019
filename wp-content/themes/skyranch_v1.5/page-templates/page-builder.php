e6d08aebc2e085bf20df10524eac6f7a6fa77a9e
Bryan <bdavis@artandbusinessone.com>
2019-09-26 11:24:58 -0600

    Updates to builder detail page to look for a gallery shortcode and publish if present. Stylesheet updates

--- wp-content/themes/skyranch_v1.5/page-templates/page-builder.php
+++ wp-content/themes/skyranch_v1.5/page-templates/page-builder.php
@@ -15,6 +15,12 @@
             <p class="home-details">
               <?php echo get_field('homebuilder_pricing') . ' | ' . get_field('homebuilder_details') ?>
             </p>
+
+            <p class="builder-link">
+              <a href="<?php echo get_field('homebuilder_link') ?>" class="btn outline-btn" target="_blank">
+                Learn more about <?php the_title() ?> at <?php bloginfo('name') ?>
+              </a>
+            </p>
           </div>
         </div>
       </div>
@@ -27,20 +33,11 @@
     <?php if(do_shortcode('[envira-gallery slug="'.$post->post_name.'"]') != ''): ?>
     <div class="col-12">
       <div class="builder-gallery-contents" id="gallery">
-        <h1><?php the_title(); echo ' Photo Gallery'; ?></h1>
+        <h1 class="gallery-title"><?php the_title(); echo ' Photo Gallery'; ?></h1>
         <?php echo do_shortcode('[envira-gallery slug="'.$post->post_name.'"]') ?>
       </div>
     </div>
     <?php endif; ?>
-
-
-      <div class="col-12">
-      <p class="builder-link">
-        <a href="<?php echo get_field('homebuilder_link') ?>" class="btn outline-btn" target="_blank">
-          Learn more about <?php the_title() ?> at <?php bloginfo('name') ?>
-        </a>
-      </p>
-      </div>
   </div>
 
 <?php get_footer() ?>
